c681eacf54d6246a908a2082655847f09d1d6a4a
Erfan <erfanjaliliyan83@gmail.com>
2026-01-07 07:28:19 -0800

    add the meta tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
 import HomePage from "./HomePage/HomePage";
+
+export const metadata = {
+  other: {
+    enamad: "48702043",
+  },
+};
 
 export default function Home() {
   return (
